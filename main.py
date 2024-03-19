a7dd264fa8b9415f416a3608310ec2cd3e0ca6ae
Sherlockc0d3r <contatogabrielantunes22@gmail.com>
2024-03-18 22:37:00 -0300

    Minha primeira aula em python

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-print("Hello World!")
-print("Sejam Bem-Vindos")
+#print("Hello World!")
+#print("Sejam Bem-Vindos")
+
+#media_final = int(input("Digite sua média final: "))
+#if media_final >= 70:
+#    print("APROVADO!")
+#else:
+#    print("REPROVADO!")
+
+vel_car = int(input("Digite a velocidade do seu carro em km/h: " ))
+multa = int(vel_car - 80) * 5
+if vel_car > 80:
+    print(f"Você foi multado! R$ {multa:.2f}.")
+else:
+    print("Sai pra lá vovó!")
